ae9404a198211772ff2ee098d987c37e434ce0e4
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-02-17 21:00:27 +0100

    It works with resizing!
    Now to fix memory leaks

--- hash_map.c
+++ hash_map.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+
+
+
 typedef struct {
   size_t size;
   size_t capacity;
@@ -11,8 +14,8 @@ typedef struct {
 
 
 HashTable *HashTable_new(size_t const capacity) {
-  HashTable *hash_table = malloc(sizeof(HashTable));
-  hash_table->capacity = capacity;
+  HashTable *hash_table = calloc(1, sizeof(HashTable));
+  hash_table->capacity = capacity > 2 ? capacity : 2;
   hash_table->size = 0;
   hash_table->keys = calloc(capacity, sizeof(void *));
   hash_table->values = calloc(capacity, sizeof(void *));
@@ -28,31 +31,33 @@ void HashTable_destroy(HashTable *hash_table) {
 
 size_t HashTable_index (HashTable const *hash_table, void *const key) {
   size_t index = ((size_t) key) % hash_table->capacity; // TODO Hashing function!
-  while (hash_table->keys[index] && hash_table->keys[index] != key) {
+  while (hash_table->keys[index] != NULL && hash_table->keys[index] != key) {
+    printf("HTC: %lu, Not storing %s at key %lu because already filled by %s => %s\n", hash_table->capacity, key, index, hash_table->keys[index], hash_table->values[index]);
     index = (index + 1) % hash_table->capacity;
   }
   return index;
 }
 
-void HashTable_insert(HashTable *hash_table, void *const key, void *const value) {
+void HashTable_insert(HashTable **hash_table_ref, void *const key, void *const value) {
+  HashTable *hash_table = *hash_table_ref;
   size_t const index = HashTable_index(hash_table, key);
+  printf("HTC: %lu, Inserting %s at index %lu\n", hash_table->capacity, key, index);
   hash_table->keys[index] = key;
   hash_table->values[index] = value;
   // TODO resizing.
 
   ++(hash_table->size);
   if(hash_table->size * 4 >= hash_table->capacity * 3) { // 3/4 maximum load factor.
-    HashTable *larger_table = HashTable_new(hash_table->capacity << 2);
+    printf("Resizing HashTable...\n");
+    HashTable *larger_table = HashTable_new(hash_table->capacity << 1);
     for(size_t iter_index = 0; iter_index < hash_table->capacity; ++iter_index) {
       if(hash_table->keys[iter_index] != NULL){
-        HashTable_insert(larger_table, hash_table->keys[iter_index], hash_table->values[iter_index]);
+        HashTable_insert(&larger_table, hash_table->keys[iter_index], hash_table->values[iter_index]);
       }
     }
-    free(hash_table->values);
-    free(hash_table->keys);
-    HashTable *tmp = hash_table;
-    *hash_table = *larger_table;
-    HashTable_destroy(tmp);
+    *hash_table_ref = larger_table;
+    /* HashTable_destroy(hash_table); */
+    printf("Finished Resizing HashTable.\n");
   }
 }
 
@@ -63,13 +68,13 @@ void *HashTable_lookup(HashTable *hash_table, void *const key) {
 
 
 int main() {
-  HashTable *hash_table = HashTable_new(10);
-  HashTable_insert(hash_table, "hello", "world");
-  HashTable_insert(hash_table, "foo", "bar");
-  HashTable_insert(hash_table, "foo2", "bar2");
-  HashTable_insert(hash_table, "foo3", "bar3");
-  HashTable_insert(hash_table, "foo4", "bar4");
-  HashTable_insert(hash_table, "foo5", "bar5");
+  HashTable *hash_table = HashTable_new(1);
+  HashTable_insert(&hash_table, "hello", "world");
+  HashTable_insert(&hash_table, "foo", "bar");
+  HashTable_insert(&hash_table, "foo2", "bar2");
+  HashTable_insert(&hash_table, "foo3", "bar3");
+  HashTable_insert(&hash_table, "foo4", "bar4");
+  HashTable_insert(&hash_table, "foo5", "bar5");
   printf("hello => %s\n", HashTable_lookup(hash_table, "hello"));
   printf("foo => %s\n", HashTable_lookup(hash_table, "foo"));
   printf("foo2 => %s\n", HashTable_lookup(hash_table, "foo2"));
